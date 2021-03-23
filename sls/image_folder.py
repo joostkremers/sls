82cee07c9cbf351cbe7147bc57ccaaab53e1c076
Joost Kremers <joostkremers@fastmail.fm>
2021-03-23 08:50:12 +0100

    Initial commit.

---
+++ sls/image_folder.py
@@ -0,0 +1,161 @@
+import os
+import hashlib
+import base64
+
+from appdirs import AppDirs
+from PIL import Image as PILImage
+
+
+class ImageFolder:
+    """A directory containing image files.
+
+    The absolute directory path is stored in the root attribute. The contents of
+    the directory are listed in the `contents` attribute. This is a dictionary
+    with the paths of all the directories under the root (including the root
+    itself) as keys, mapped onto 2-tuples consisting of a list of the
+    subdirectories and a list of the files in each directory. The paths used as
+    keys are relative paths starting from `root`, which means that the root
+    itself is stored under the entry '.'.
+
+    When an ImageFolder is instantiated, a thumbnail directory is created in the
+    user's cache dir if one does not already exists.
+
+    Attributes
+    ----------
+    root : str
+        File path of the image directory.
+    contents : Dict[str, str]
+        Dictionary of directory paths to their contents.
+    dirs: AppDirs
+        App-specific directories
+    thumbnail_dir : str
+        File path of the thumbnail directory.
+
+    """
+
+    def __init__(self, root: str):
+        """
+        Create a ImageFolder instance.
+
+        Parameters
+        ----------
+        root : str
+            File path of the image directory.
+        """
+
+        # Set root, contents and dirs attributes.
+        self.root = os.path.expanduser(root)
+        self.contents = {}
+        for directory, subdirs, files in os.walk(self.root):
+            rel_dir = os.path.relpath(directory, self.root)
+            self.contents[rel_dir] = (subdirs, files)
+
+        self.dirs = AppDirs("sls", "ten.eleven")
+
+        # Set thumbnail_dir attribute.
+        self.thumbnail_dir = os.path.join(
+            self.dirs.user_cache_dir,
+            ImageFolder.generate_dir_name(self.root),
+            "thumbnails",
+        )
+        os.makedirs(self.thumbnail_dir, exist_ok=True)
+
+    def __str__(self):
+        result = ""
+        for key, val in self.contents.items():
+            result += f"{key}: {val}\n"
+
+        return result
+
+    @classmethod
+    def generate_dir_name(cls, path):
+        """Generate a unique directory name based on `path`.
+
+        Return a directory name consisting of the base name of `path` followed
+        by the first 8 characters of the base64-encoded sha256 hash of `path`.
+        This should ensure that if we create two ImageFolder objects with the
+        same base name but different paths, they won't collide.
+
+        Parameters
+        ----------
+        path : str
+            Path to generate a directory name for.
+
+        Returns
+        -------
+        str
+            Generated base name.
+
+        """
+        d = os.path.basename(path)
+        h = hashlib.sha256(path.encode())
+        b = base64.urlsafe_b64encode(h.digest()).decode()[:8]
+
+        return f"{d}-{b}"
+
+    def first_image(self, folder: str):
+        """Return the first image in the folder.
+
+        Take the first picture in folder and return its absolute file path. The
+        returned file is the first file listed for the folder in self.contents.
+        If this folder does not contain any images, return the stock image
+        'missing_image.png'.
+
+        Parameters
+        ----------
+        folder : str
+            Relative path of the folder, starting from `self.root`.
+
+        Returns
+        -------
+        str
+            Relative path of the image, starting from `self.root`.
+
+        """
+
+        files = self.contents[folder][1]
+        if files:
+            return os.path.join(folder, files[0])
+        else:
+            return "../Images/missing_image.png"
+
+    def create_thumbnail(self, image_path: str) -> str:
+        """Create a thumbnail for `image` and return its file path.
+
+        If the thumbnail already exists, just return its path. If the thumbnail
+        cannot be created, return the path to the stock image
+        'missing_image.png'.
+
+        Parameters
+        ----------
+        image_path : str
+            Path of the image, relative to `self.root`.
+
+        Returns
+        -------
+        str
+            Absolute file path of the thumbnail.
+
+        """
+        thumbnail_path = os.path.join(self.thumbnail_dir, image_path)
+
+        # https://openclipart.org/detail/298746/missing-image
+
+        try:
+            if os.path.exists(thumbnail_path):
+                return thumbnail_path
+            with PILImage.open(os.path.join(self.root, image_path)) as image:
+                thumbnail_dir = os.path.dirname(thumbnail_path)
+                if not os.path.exists(thumbnail_dir):
+                    os.makedirs(thumbnail_dir)
+
+                image.thumbnail((500, 500))
+                image.save(thumbnail_path)
+        except OSError:
+            thumbnail_path = "../Images/missing_image.png"
+
+        return thumbnail_path
+
+
+if __name__ == "__main__":
+    imgfldr = ImageFolder("~/src/Python/sls/Pictures")
---
+++ sls/sls.py
@@ -0,0 +1,97 @@
+import os.path
+from typing import List
+
+from kivy.app import App
+
+from kivy.uix.boxlayout import BoxLayout
+
+from kivy.properties import ObjectProperty, ListProperty
+
+from sls.image_folder import ImageFolder
+
+
+class SLSView(BoxLayout):
+    """The main window of the program.
+
+    The SLSView contains a MenuBar and a RecycleView that holds the actual
+    images..
+
+    Attributes
+    ----------
+    folder : ImageFolder
+        The directory for which images are displayed.
+    view : RecycleView
+        The RecycleView instance holding the images.
+
+    """
+
+    view = ObjectProperty()
+
+    @staticmethod
+    def chunk(lst: List, chunk_size: int):
+        for i in range(0, len(lst), chunk_size):
+            yield lst[i : i + chunk_size]
+
+    def __init__(self, **kwargs):
+        super(SLSView, self).__init__(**kwargs)
+
+        self.folder = ImageFolder("~/src/Python/sls/Pictures")
+        # We pass "" as the `directory` argument of `add_section`, not '.',
+        # because the directory is combined with the names of its subdir using
+        # `os.path.join`. With '.', this leads to paths like "./subdir", which
+        # subsequently cannot be found in `self.folder.contents` when
+        # `add_section` creates sections for the subdir.
+        self.add_folder("", *self.folder.contents["."])
+
+        # root.ids.app_title.text = self.folder.root
+
+    def add_folder(self, directory: str, subdirs: List[str], files: List[str]):
+        """Add the contents of a directory to the SLSView.
+
+        Parameters
+        ----------
+        directory : str
+            Path of the directory to be added, relative to `self.folder.root`.
+        subdirs : List[str]
+            Paths of the subdirs in `directory`.
+        files : List[str]
+            The files in `directory`.
+
+        """
+
+        if files:
+            self.view.data.append(self.create_label(directory))
+            rows = self.chunk(files, 3)
+            for row in rows:
+                self.view.data.append(
+                    self.create_image_row(
+                        [os.path.join(directory, file) for file in row]
+                    )
+                )
+
+        for subdir in subdirs:
+            subdir_path = os.path.join(directory, subdir)
+            self.view.data.append(self.create_label(subdir_path))
+            self.view.data.append(self.create_folder(subdir_path))
+
+    def create_label(self, path: str):
+        pretty_path = path.replace(os.path.sep, " › ")
+        return {"widget": "SLSFolderLabel", "path": pretty_path}
+
+    def create_image_row(self, images: List[str]):
+        thumbnails = [self.folder.create_thumbnail(file) for file in images]
+        return {"widget": "SLSImageRowM", "images": thumbnails}
+
+    def create_folder(self, path: str):
+        image_path = self.folder.first_image(path)
+        thumbnail = self.folder.create_thumbnail(image_path)
+        return {"widget": "SLSFolder", "source": thumbnail}
+
+
+class SLSApp(App):
+    def build(self):
+        return SLSView()
+
+
+if __name__ == "__main__":
+    SLSApp().run()
